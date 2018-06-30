937ffa22f8cb0effaf27ce5cc9df8ac5b951479a
MitsunoriS <>
2018-06-30 09:37:18 +0900

    add actions for new&edit views

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,24 @@
 class GroupsController < ApplicationController
+  def new
+  end
+
+  def create
+    Group.create(name: group_params[:name], user_id: current_user.id)
+    redirect_to "groups/#{group_id}/messages"
+  end
+
+  def edit
+    @group = group.find(params[:id])
+  end
+
+  def update
+
+    redirect_to "groups/#{group_id}/messages"
+  end
+
+  private
+  def group_params
+    params.parmit(:name)
+  end
+
 end
