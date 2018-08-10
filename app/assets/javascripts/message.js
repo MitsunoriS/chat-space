605b99115e6a9ab2147b522af6d1f9145f8703ee
MitsunoriS <>
2018-08-10 10:52:44 +0900

    Write a JavaScript for Ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,52 @@
 $(function(){
-  $('#new_message').on('submit', function(e){
+  function buildHTML(message){
+      var html = `<div class="message">
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${ message.user_name }
+                      </div>
+                      <div class="upper-message__date">
+                        ${ message.time }
+                      </div>
+                    </div>
+                    <div class="lower-meesage">
+                      <p class="lower-message__content">
+                      ${ message.message }
+                      </p>
+                      <div class="lower-message__image">
+                        ${ message.image }
+                      </div>
+                    </div>
+                  </div>`
+      return html;
+  }
+
+  function scroll_to_bottom(){
+    $('.body').animate({scrollTop: $('.body__chat_box')[0].scrollHeight}, 500);
+  }
+
+  $('#new_message').on('submit', function(e) {
     e.preventDefault();
-    console.log(this)
     var formData = new FormData(this);
-  })
-})
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.body__chat_box').append(html)
+      $('.new_message')[0].reset()
+      scroll_to_bottom();
+    })
+    .fail(function() {
+      alert('error');
+    });
+  });
+});
+
