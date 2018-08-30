8eef5a0d151993faf60cf704bdeea8a5cf285731
MitsunoriS <mitsus@Mitsu-Mac.local>
2018-08-30 16:48:27 +0900

    Edit more files by auto-rubocop

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '52.199.226.35', user: 'ec2-user', roles: %w{app db web}
+server '52.199.226.35', user: 'ec2-user', roles: %w[app db web]
 
 # role-based syntax
 # ==================
